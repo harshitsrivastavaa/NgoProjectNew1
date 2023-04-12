ec330609c190f9100ec2363ea999ca5e18734b5c
harshsri <harshsri@LIN80029172>
2023-04-12 13:56:21 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NgoProjectNew1.Models;
+using NgoProjectNew1.Models.ViewModel;
+
+
+namespace NgoProjectNew1.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        //private readonly ILogger<HomeController> _logger;
+        private readonly NgoDbContext _context;
+
+        public HomeController(NgoDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginSingupViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var data = _context.NgoRegMembers.Where(e => e.Username == model.Username);
+                if (data != null)
+                {
+                    bool isValid = (data.FirstOrDefault().Username == model.Username && data.FirstOrDefault().Password == model.Password);
+                    if (isValid)
+                    {
+                        var identity = new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, model.Username) },
+                            CookieAuthenticationDefaults.AuthenticationScheme);
+                        var principal = new ClaimsPrincipal(identity);
+                        HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+                        HttpContext.Session.SetString("Username", model.Username);
+                        return RedirectToAction("Index", "Home");
+
+                    }
+                    else
+                    {
+                        TempData["errorPassword"] = "Invalid Password!";
+                        return View(model);
+                        
+                    }
+
+                }
+                else
+                {
+                    TempData["errorUsername"] = "Invalid Username";
+                    return View(model);
+                }
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+        public IActionResult LogOut()
+        {
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Home");
+        }
+
+        [AcceptVerbs("Post","Get")]
+        public IActionResult UserNameIsExist(string userName)
+        {
+            var data = _context.NgoRegMembers.Where(e => e.Username == userName).SingleOrDefault();
+            if (data != null)
+            {
+                return Json($"Username {userName} already exist");
+            }
+            else
+            {
+                return Json(true);
+            }
+        }
+        public IActionResult SignUp()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(SignUpViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var data = new NgoRegMember()
+                { 
+                    Username = model.Username,
+                    ContactNo = model.ContactNo,
+                    Address = model.Address,
+                    Name = model.Name,
+                    Password=model.Password
+                };
+                _context.NgoRegMembers.Add(data);
+                _context.SaveChanges();
+                TempData["successMessage"] = "Registartion done successfully";
+                return RedirectToAction("Login");
+            }
+            else
+            {
+                TempData["errorMessage"] = "Empty form can't be submitted!";
+                return View(model);
+            }
+        }
+
+       
+  
+     }
+ }
+
---
+++ Models/ViewModel/SignUpViewModel.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NgoProjectNew1.Models.ViewModel
+{
+    public class SignUpViewModel
+    {
+        [Required(ErrorMessage = "Name is Required")]
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Mobile is required")]
+        [RegularExpression(@"\d{10}", ErrorMessage = "Please enter 10 digit Mobile No.")]
+        public string ContactNo { get; set; }
+
+        [Required(ErrorMessage = "UserName is Required")]
+        [Remote(action:"UserNameIsExist",controller:"Home")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
