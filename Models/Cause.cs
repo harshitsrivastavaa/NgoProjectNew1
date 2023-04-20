50d7125f17a82ee0ababa916a341c30305fc9590
Srivastava <harshit.b.srivastava@capgemini.com>
2023-04-20 16:01:39 +0530

    Pushed today

---
+++ Models/Cause.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace NgoProjectNew1.Models
+{
+    public partial class Cause
+    {
+        public int CauseId { get; set; }
+        public string RaiserName { get; set; }
+        public string CauseName { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string Category { get; set; }
+        public string Contact { get; set; }
+        public string CauseDesc { get; set; }
+        public string PickUpAddress { get; set; }
+        public int? RaiserCount { get; set; }
+
+        public virtual NgoRegMember CauseNavigation { get; set; }
+    }
+}
---
+++ Models/ViewModel/CausesViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NgoProjectNew1.Models.ViewModel
+{
+    public class CausesViewModel
+    {
+        public string RaiserName { get; set; }
+        public string CauseName { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string Category { get; set; }
+        public string Contact { get; set; }
+        public string CauseDesc { get; set; }
+        public string PickUpAddress { get; set; }
+        public int? RaiserCount { get; set; }
+
+        public class SearchResult
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            // Add additional properties as needed
+        }
+
+    }
+}
